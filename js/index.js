77f6bae4ff6da0a8b314e3bd8ed43d83997d05de
Steven Pickering <52013044+Steven909@users.noreply.github.com>
2021-11-09 16:34:49 +0000

    Deleting fact

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@
 const FCC = "freeCodeCamp"; 
 let fact = "is cool!"; 
 fact = "is awesome!";
-fact = "is shite... not!";
+fact = "is infact the best";
 console.log(FCC, fact);
 
 const difference = 45 - 0;

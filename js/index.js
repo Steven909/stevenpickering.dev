2bd7fdfc821bbfdc9e2c4492c1741aa672680491
Steven Pickering <52013044+Steven909@users.noreply.github.com>
2021-11-09 16:32:48 +0000

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+
+// some test javascript from
+const FCC = "freeCodeCamp"; 
+let fact = "is cool!"; 
+fact = "is awesome!";
+fact = "is shite... not!";
+console.log(FCC, fact);
+
+const difference = 45 - 0;
+const remainder = 11 % 3;
+console.log(remainder);
+
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; 
+console.log(myStr);
+
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
